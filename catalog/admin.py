0e79a1e2eea6ca46e4d165ce1e42ecef04c41b4e
Connor Murphy <connor@murphysites.com>
2024-01-06 21:59:36 -0500

    register everything in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 
-from .models import ReleaseGroup, DetailType, Detail
+from .models import ReleaseGroup, DetailType, Detail, Release, Artist, Track
+
+class ReleaseInline(admin.StackedInline):
+    model = Release
+    extra = 1
+
+class DetailInline(admin.TabularInline):
+    model = Detail
+    extra = 3
+    
+class TrackInline(admin.TabularInline):
+    model = Track
+    extra = 3
+
+class ReleaseGroupAdmin(admin.ModelAdmin):
+    # list_display = ["cat_id","name","date","form"]
+    fieldsets = [
+        ("Release Number", {"fields": ["num"]}),
+        (None, {"fields": ["name"]}),
+        ("Release Date", {"fields": ["date"]}),
+    ]
+    
+    inlines = [ReleaseInline, DetailInline, TrackInline]
 
 # Register your models here.
-admin.site.register(ReleaseGroup)
+admin.site.register(ReleaseGroup, ReleaseGroupAdmin)
+admin.site.register(Track)
 admin.site.register(Detail)
 admin.site.register(DetailType)
+admin.site.register(Artist)
+admin.site.register(Release)
